9a0a817c711f6b824df23d873e3f4fe97c4eb4bb
naski :) <crackedghouL@gmail.com>
2016-06-07 18:29:22 +0100

    small note

--- scripts/commonstates/v1/TradeManager.lua
+++ scripts/commonstates/v1/TradeManager.lua
@@ -156,7 +156,7 @@ function TradeManagerState:Run()
             return
         end
         -- BDOLua.Execute("npcShop_requestList()")
-		BDOLua.Execute("HandleClickedFuncButton(getDialogButtonIndexByType(CppEnums.ContentsType.Contents_Shop))")
+		BDOLua.Execute("HandleClickedFuncButton(getDialogButtonIndexByType(CppEnums.ContentsType.Contents_Shop))") -- yes. Contents_Shop is correct.
         self.SleepTimer = PyxTimer:New(2)
         self.SleepTimer:Start()
         self.State = 4
